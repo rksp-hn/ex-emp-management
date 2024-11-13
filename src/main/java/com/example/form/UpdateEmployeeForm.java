9a1538a5ac2cf9c0df2def245d005a2b92ad6934
HN <hiroyuki.nagashima@rakus-partners.co.jp>
2024-11-13 11:50:36 +0900

    updateEmployeeForm

--- src/main/java/com/example/form/UpdateEmployeeForm.java
+++ src/main/java/com/example/form/UpdateEmployeeForm.java
@@ -2,4 +2,23 @@ package com.example.form;
 
 public class UpdateEmployeeForm {
 
+    private String id; // 従業員ID
+    private String dependentsCount; // 扶養人数
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getDependentsCount() {
+        return dependentsCount;
+    }
+    public void setDependentsCount(String dependentsCount) {
+        this.dependentsCount = dependentsCount;
+    }
+    @Override
+    public String toString() {
+        return "UpdateEmployeeForm [id=" + id + ", dependentsCount=" + dependentsCount + "]";
+    }
+    
 }
